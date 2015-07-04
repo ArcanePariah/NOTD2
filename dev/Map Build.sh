d8d9999783a7f75127a6136f3ffa3bf000d6291f
Eric Nelson <durandel12345@gmail.com>
2015-07-04 15:58:49 -0700

    Revised Map Build script to include terrain changes

--- dev/Map Build.sh
+++ dev/Map Build.sh
@@ -2,3 +2,5 @@ add "../build/Civilian Campaign.SC2Map" "./Civilian Campaign.SC2Map/*" /r /auto
 f "../build/Civilian Campaign.SC2Map"
 add "../build/NOTD 2 Sub Missions.SC2Map" "./NOTD 2 Sub Missions.SC2Map/*" /r /auto
 f "../build/NOTD 2 Sub Missions.SC2Map.SC2Map"
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/t3*" /r /auto
+f "../build/NOTD 2.SC2Map.SC2Map"
