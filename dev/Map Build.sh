455e8b2ea711de4e6071787bb06f28893415918e
Eric Nelson <durandel12345@gmail.com>
2015-07-11 13:34:03 -0700

    Refined map build script

--- dev/Map Build.sh
+++ dev/Map Build.sh
@@ -3,4 +3,11 @@ f "../build/Civilian Campaign.SC2Map"
 add "../build/NOTD 2 Sub Missions.SC2Map" "./NOTD 2 Sub Missions.SC2Map/*" /r /auto
 f "../build/NOTD 2 Sub Missions.SC2Map.SC2Map"
 add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/t3*" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/Objects*" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/Regions*" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/Attributes*" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/MapInfo*" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/PaintedPathingLayer" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/PaintedCreepLayer" /r /auto
+add "../build/NOTD 2.SC2Map" "./NOTD 2.SC2Map/*.xml" /r /auto
 f "../build/NOTD 2.SC2Map.SC2Map"
